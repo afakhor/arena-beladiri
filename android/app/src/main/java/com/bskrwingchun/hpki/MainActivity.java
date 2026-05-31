29ac143299c8c4ba03cce0cf6e26abb0b04534ed
afakhor <31812946+afakhor@users.noreply.github.com>
2026-05-31 18:46:13 +0700

    Delete MainActivity.java

--- android/app/src/main/java/com/bskrwingchun/hpki/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.heruwingchun.hpki; // Sesuaikan dengan nama paket baru Coach
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
